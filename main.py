a05f3d98c0cb4821f09fb1166bfc7cb1ad403697
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-12-04 12:32:20 +0330

    adding entry and button

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+ 
+window = Tk()
+window.title("Mile to Km convertor")
+window.minsize(450, 450)
+window.config(padx=30, pady=30)
+
+entrty = Entry(width=20)
+entrty.grid(column=0, row=0)
+
+# lable = Label(text="Miles is equal to")
+# lable.grid(column=1, row=0)
+
+
+
+
+window.mainloop()
